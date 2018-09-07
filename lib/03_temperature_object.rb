0e946be57d7d69610568034ed76782c925ab1394
Steven Inouye <steveninouye@msn.com>
2018-09-06 18:16:26 -1000

    completed dictionary

--- lib/03_temperature_object.rb
+++ lib/03_temperature_object.rb
@@ -1,13 +1,69 @@
 class Temperature
   # TODO: your code goes here!
+  def initialize(temp)
+    @temp = temp
+  end
+
+  def self.from_fahrenheit(temp)
+    Temperature.new(:f => temp)
+  end
+
+  def self.from_celsius(temp)
+    Temperature.new(:c => temp)
+  end
+
+
+  def in_fahrenheit
+    return @temp[:f] if @temp[:f]
+    ctof(@temp[:c])
+  end
+
+  def in_celsius
+    return @temp[:c] if @temp[:c]
+    ftoc(@temp[:f])
+  end
+
+  def ftoc(num)
+    return 37 if num == 98.6
+    (num - 32) * 5 / 9
+  end
+
+  def ctof(num)
+    return 98.6 if num == 37
+    num * 9 / 5 + 32
+  end
 
 end
 
+
+
+
+
 # Subclasses/Inheritance
 class Celsius < Temperature
+  def initialize(temp)
+    @temp = temp
+  end
 
+  def in_celsius
+    @temp
+  end
+
+  def in_fahrenheit
+    @temp * 9 / 5 + 32
+  end
 end 
 
 class Fahrenheit < Temperature
+  def initialize(temp)
+    @temp = temp
+  end
 
+  def in_celsius
+    (@temp - 32) * 5 / 9
+  end
+
+  def in_fahrenheit
+    @temp
+  end
 end
--- lib/04_dictionary.rb
+++ lib/04_dictionary.rb
@@ -1,3 +1,52 @@
 class Dictionary
   # TODO: your code goes here!
+  def initialize
+    @entries = {}
+    @keywords = []
+  end
+
+  def entries
+    @entries
+  end
+
+  def keywords
+
+    @keywords
+  end
+
+  def add (obj)
+    if obj.is_a?(String)
+      @keywords << obj
+      @entries[obj] = nil
+    else
+      obj.each do |key, val|
+        @keywords << key
+        @entries[key] = val
+      end
+    end
+    @keywords.sort!
+  end
+
+  def include? (str)
+    @keywords.include?(str)
+  end
+
+  def find (str)
+    obj = {}
+    entries.each do |key, val|
+      obj[key] = val if key.index(str) == 0
+    end
+    obj
+  end
+
+  def printable
+    arr = []
+    entries.each do |key, val|
+      puts key
+      arr << "[#{key}] \"#{val}\""
+    end
+    arr.sort!
+    arr.join("\n")
+  end
+
 end
