7ffd8aa1f9b5a049e7edbfc448ec249ae73d74bb
Steven Inouye <steveninouye@msn.com>
2018-09-06 15:07:19 -1000

    completed book titles

--- lib/01_book_titles.rb
+++ lib/01_book_titles.rb
@@ -1,3 +1,20 @@
 class Book
   # TODO: your code goes here!
+
+
+  def title
+    @title
+  end
+
+  def title=(title)
+    ignr = ["the","a","an","and", "in", "of"]
+    title_arr = title.split.map.with_index do |word, idx|
+      if ignr.include?(word) && idx != 0
+        word.downcase
+      else
+        word.capitalize
+      end
+    end
+    @title = title_arr.join(" ")
+  end
 end
